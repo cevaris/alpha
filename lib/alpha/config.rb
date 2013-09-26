3d75f7b64e9364b125dd973ba0e1d34984503db1
James Kobol <jim.kobol@gmail.com>
2013-09-26 15:12:27 -0600

    can read config file now in test

--- lib/alpha/config.rb
+++ lib/alpha/config.rb
@@ -4,8 +4,7 @@ module Alpha
 		def load (path)
 			@config_path = path
 			@config_file = File.open( @config_path )
-			@config_data = YAML::parse( @config_file )
-			p @config_data[:database]
+			@config_data = YAML::load( @config_file )
 		end
 	end
 end
--- spec/alpha_config.rb
+++ spec/alpha_config.rb
@@ -1,11 +1,23 @@
 require 'alpha'
 
 describe Alpha::Create do
+
+	before :each do 
+		@path = 'config/config.yaml'
+		@config = Alpha::Config.new
+	end
   
-  it 'cannot find config file' do
-    config = Alpha::Config.new
-    path = 'config/config.yaml'
-    config.load path
+  it 'cannot find config file did not throw error' do
+    
+  	path = 'invalid/config/file.yaml'
+  	expect { @config.load path }.to raise_error
+
+  end
+
+  it 'cannot read config file' do
+    # path = 'config/config.yaml'
+    config_data = @config.load @path
+    config_data.should_not eq( nil )
   end
 
 end
