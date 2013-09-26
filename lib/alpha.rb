2219102c6aff67ab0edb8550d488b5b938626f1e
James Kobol <jim.kobol@gmail.com>
2013-09-25 23:15:58 -0600

    deleted runner

--- lib/alpha.rb
+++ lib/alpha.rb
@@ -1,5 +1,6 @@
 require "alpha/version"
+require "alpha/create"
 
-module Alpha
-  # Your code goes here...
-end
+# module Alpha
+#   # Your code goes here...
+# end
---
+++ lib/alpha/create.rb
@@ -0,0 +1,3 @@
+module Alpha
+	CREATE = 'You gots it man'
+end
